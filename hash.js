af7a7d83ae03d9d3eff61a27ba004702cb9d6d50
sachkosm <39930413+sachkosm@users.noreply.github.com>
2019-07-14 19:25:53 -0400

    Update and rename hash to hash.js

--- hash.js
+++ hash.js
@@ -10,3 +10,26 @@ A valid hash for a blockchain is a hash that meets a certain requirement.
 For this blockchain, having three zeros at the beginning of the hash is the requirement for a valid hash.
 
 The number of leading zeros required is the 'difficulty'.
+
+// cosnt Block = reuqire("./Block.js");
+
+// class Blockchain {
+  // constructor() {
+    // this.blockchain = [Block.genesis()];
+    this.difficulty = 3;
+  // }
+  
+  // get() { ... }
+  // get latestBlock() { ... }
+
+  isValidHashDifficulty(hash) {
+    for (var i = 0; i < hash.length; i++) {
+      if (hash[i] !== "0") {
+        break;
+      };
+    }
+    return i >= this.difficulty;
+  }
+// };
+
+// module.exports = Blockchain;
